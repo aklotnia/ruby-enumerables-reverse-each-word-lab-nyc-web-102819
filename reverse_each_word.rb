b4cde46a24f09d6a9a6a9cd4f74f56fd3a9d0b7d
Aaron Klotnia <aklotnia@gmail.com>
2019-10-10 19:49:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,6 @@
 def reverse_each_word(sent)
   sent_array = sent.split(" ")
-  sent_array.map{|word| word.reverse}
+  rev_array = sent_array.map{|word| word.reverse}
+  brand_new_sentence = " ".join(rev_array)
+  brand_new_sentence
+end
