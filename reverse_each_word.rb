baa6290e1a6fc4af9afaa1eb9f36102c93311f8b
Aaron Klotnia <aklotnia@gmail.com>
2019-10-10 19:54:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,15 +6,13 @@
 # end
 
 
-rev_array = []
-sent_array.each do |word|
-  rev_word = word.reverse
-  rev_array << rev_word
-end
-
 def reverse_each_word(sent)
   sent_array = sent.split(" ")
-  rev_array = sent_array.collect{|word| word.reverse}
+  rev_array = []
+  sent_array.each do |word|
+    rev_word = word.reverse
+    rev_array << rev_word
+  end
   brand_new_sentence = rev_array.join(" ")
   brand_new_sentence
 end  
