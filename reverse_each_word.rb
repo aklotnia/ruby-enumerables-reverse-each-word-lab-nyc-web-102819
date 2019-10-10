d5e184a83a41aabaef1bfe920a37f3f8c41cf6e6
Aaron Klotnia <aklotnia@gmail.com>
2019-10-10 19:54:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,7 @@
-# def reverse_each_word(sent)
-#   sent_array = sent.split(" ")
-#   rev_array = sent_array.collect{|word| word.reverse}
-#   brand_new_sentence = rev_array.join(" ")
-#   brand_new_sentence
-# end
-
-
 def reverse_each_word(sent)
   sent_array = sent.split(" ")
-  rev_array = []
-  sent_array.each do |word|
-    rev_word = word.reverse
-    rev_array << rev_word
-  end
+  rev_array = sent_array.collect{|word| word.reverse}
   brand_new_sentence = rev_array.join(" ")
   brand_new_sentence
-end  
+end
+
